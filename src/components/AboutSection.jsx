06cfe95afe98636300299c5806692a853167ae39
rithishvellingiri <rithishvellingiri@gmail.com>
2025-12-05 15:20:00 +0530

    resume updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1swvSYDvuV5bAzrTyD9D-GtuhoxPS5MTo/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1q-cuT13H6jl2kIIVqZRXr84EwL0GKjOS/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
